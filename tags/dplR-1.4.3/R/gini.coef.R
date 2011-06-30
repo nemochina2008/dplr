9cf6f1e52dec3ee9aef0ef2aa0428892c574dfc2
mvkorpel <mvkorpel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-30 11:05:42 +0000

    trying to figure this out
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dplr@198 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tags/dplR-1.4.3/R/gini.coef.R
+++
@@ -1,6 +0,0 @@
-`gini.coef` <- function(x)
-{
-    x <- as.double(x[!is.na(x)])
-    .C(dplR.gini,
-       x, as.integer(length(x)), result=NaN, NAOK=TRUE, DUP=FALSE)$result
-}
--- tags/dplR-1.4.3/dplR/R/sens1.R
+++
@@ -1,6 +0,0 @@
-`sens1` <- function(x)
-{
-    x <- as.double(x[!is.na(x)])
-    .C(dplR.sens1,
-       x, as.integer(length(x)), result=NaN, NAOK=TRUE, DUP=FALSE)$result
-}
