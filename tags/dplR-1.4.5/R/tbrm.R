36620ac47270b954b20aaba6560541817a3eb8c4
mvkorpel <mvkorpel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-11 11:35:23 +0000

    Noe yet...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dplr@271 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tags/dplR-1.4.5/R/tbrm.R
+++
@@ -1,7 +0,0 @@
-`tbrm` <- function(x, C=9)
-{
-    y <- as.double(x[!is.na(x)])
-    .C(dplR.tbrm,
-       y, as.integer(length(y)), as.double(C),
-       result=NaN, NAOK=TRUE, DUP=FALSE)$result
-}
