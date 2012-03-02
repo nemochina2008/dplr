0f2dfd9a762101e2d03b6094028a44bdd9193c11
mvkorpel <mvkorpel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-02 00:21:04 +0000

    more zero.is.missing detrending. largely untested...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dplr@484 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/zero-is-missing/R/detrend.series.R
+++ branches/zero-is-missing/R/detrend.series.R
@@ -22,50 +22,52 @@
         stop("'NA's are not allowed in the middle of the series")
     }
     y2 <- y[seq(from = good.y[1], to = good.y[n.good], by = 1)]
+    n.y2 <- length(y2)
     if (zero.is.missing) {
         good.y2 <- good.y - (good.y[1] - 1)
-        good.flag <- rep(FALSE, length(y2))
+        good.flag <- rep(FALSE, n.y2)
         good.flag[good.y2] <- TRUE
         y2[!good.flag] <- NA_real_
     } else {
         ## Recode any zero values to 0.001
         good.y2 <- seq_along(y2)
-        good.flag <- rep(TRUE, length(y2))
+        good.flag <- rep(TRUE, n.y2)
         y2[y2 == 0] <- 0.001
     }
+    y3 <- y2[good.y2]
 
     resids <- list()
 
     if ("ModNegExp" %in% method2) {
         ## Nec or lm
-        nec.func <- function(Y, flag) {
+        nec.func <- function(Y, not.na.flag, not.na.idx) {
             n <- length(Y)
             first.part <- rep(FALSE, n)
             first.part[seq_len(floor(n * 0.1))] <- TRUE
-            a <- mean(Y[first.part & flag])
+            a <- mean(Y[first.part & not.na.flag])
             b <- -0.01
             last.part <- rep(FALSE, n)
             last.part[floor(n * 0.9):n] <- TRUE
-            k <- mean(Y[last.part & flag])
-            nec <- nls(formula = Y[flag] ~ a * exp(b * which(flag)) + k,
+            k <- mean(Y[last.part & not.na.flag])
+            nec <- nls(formula = Y[not.na.idx] ~ a * exp(b * not.na.idx) + k,
                        start = list(a=a, b=b, k=k))
-            if(coef(nec)[2] >= 0) stop()
+            if (coef(nec)[2] >= 0) stop()
             fits <- predict(nec)
-            n.fits <- length(fits)
-            if(fits[1] < fits[n.fits]) stop()
-            if(fits[n.fits] < 0) stop()
+            last.fit <- fits[length(fits)]
+            if (fits[1] < last.fit) stop()
+            if (last.fit < 0) stop()
             fits
         }
-        ModNegExp <- try(nec.func(y2, good.flag), silent=TRUE)
+        ModNegExp <- try(nec.func(y2, good.flag, good.y2), silent=TRUE)
         if (class(ModNegExp)=="try-error") {
             ## Straight line via linear regression
-            lm1 <- lm(y2[good.flag] ~ which(good.flag))
+            lm1 <- lm(y3 ~ good.y2)
             ModNegExp <- predict(lm1)
             if (coef(lm1)[2] > 0 && !pos.slope) {
-                ModNegExp <- rep(mean(y2[good.flag]), n.good)
+                ModNegExp <- rep(mean(y3), n.good)
             }
         }
-        resids$ModNegExp <- y2[good.flag] / ModNegExp
+        resids$ModNegExp <- y3 / ModNegExp
         do.mne <- TRUE
     } else {
         do.mne <- FALSE
@@ -81,8 +83,8 @@
         } else {
             nyrs2 <- nyrs
         }
-        Spline <- ffcsaps(y=y2[good.flag], x=good.y2, nyrs=nyrs2, f=f)
-        resids$Spline <- y2[good.flag] / Spline
+        Spline <- ffcsaps(y=y3, x=good.y2, nyrs=nyrs2, f=f)
+        resids$Spline <- y3 / Spline
         do.spline <- TRUE
     } else {
         do.spline <- FALSE
@@ -90,8 +92,8 @@
 
     if ("Mean" %in% method2) {
         ## Fit a horiz line
-        Mean <- rep(mean(y2[good.flag]), n.good)
-        resids$Mean <- y2[good.flag] / Mean
+        Mean <- rep(mean(y3), n.good)
+        resids$Mean <- y3 / Mean
         do.mean <- TRUE
     } else {
         do.mean <- FALSE
@@ -123,7 +125,7 @@
         }
 
         if(do.spline){
-            plot.resSpline <- rep(NA_real_, length(y2))
+            plot.resSpline <- rep(NA_real_, n.y2)
             plot.resSpline[good.flag] <- resids$Spline
             plot(plot.resSpline, type="l", col="green",
                  main=gettext("Spline", domain="R-dplR"),
@@ -133,7 +135,7 @@
         }
 
         if(do.mne){
-            plot.resModnegexp <- rep(NA_real_, length(y2))
+            plot.resModnegexp <- rep(NA_real_, n.y2)
             plot.resModnegexp[good.flag] <- resids$ModNegExp
             plot(plot.resModnegexp, type="l", col="red",
                  main=gettext("Neg. Exp. Curve or Straight Line",
@@ -144,7 +146,7 @@
         }
 
         if(do.mean){
-            plot.resMean <- rep(NA_real_, length(y2))
+            plot.resMean <- rep(NA_real_, n.y2)
             plot.resMean[good.flag] <- resids$Mean
             plot(plot.resMean, type="l", col="blue",
                  main=gettext("Horizontal Line (Mean)", domain="R-dplR"),
