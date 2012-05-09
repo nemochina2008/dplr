5b6fcd92e85db84139ba837ee06d534c6941660a
mvkorpel <mvkorpel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-09 08:43:52 +0000

    handles zero-row input
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dplr@543 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/zero-is-missing/R/combine.rwl.R
+++ branches/zero-is-missing/R/combine.rwl.R
@@ -2,23 +2,42 @@ combine.rwl <- function(x, y = NULL) {
     combinator <- function(x, y) {
         dim.x2 <- ncol(x)
         dim.y2 <- ncol(y)
-        if(dim.x2 > 0 && dim.y2 > 0){
+        if (dim.x2 > 0 && dim.y2 > 0) {
+            dim.x1 <- nrow(x)
+            dim.y1 <- nrow(y)
             dim2 <- dim.x2 + dim.y2
-            years.x <- rownames(x)
-            years.y <- rownames(y)
-            min.x <- as.numeric(years.x[1])
-            min.y <- as.numeric(years.y[1])
-            max.x <- as.numeric(years.x[length(years.x)])
-            max.y <- as.numeric(years.y[length(years.y)])
-            min.year <- min(min.x, min.y)
-            years <- min.year:max(max.x, max.y)
-            new <- matrix(NA, nrow = length(years), ncol = dim2)
-            new[(min.x-min.year+1):(max.x-min.year+1), seq_len(dim.x2)] <- x
-            new[(min.y-min.year+1):(max.y-min.year+1), (dim.x2+1):dim2] <- y
-            rownames(new) <- years
-            colnames(new) <- c(colnames(x), colnames(y))
+            if (dim.x1 > 0 && dim.y1 > 0) {
+                years.x <- rownames(x)
+                years.y <- rownames(y)
+                min.x <- as.numeric(years.x[1])
+                min.y <- as.numeric(years.y[1])
+                max.x <- as.numeric(years.x[length(years.x)])
+                max.y <- as.numeric(years.y[length(years.y)])
+                min.year <- min(min.x, min.y)
+                years <- min.year:max(max.x, max.y)
+                new <- matrix(NA, nrow = length(years), ncol = dim2)
+                new[(min.x-min.year+1):(max.x-min.year+1),seq_len(dim.x2)] <- x
+                new[(min.y-min.year+1):(max.y-min.year+1),(dim.x2+1):dim2] <- y
+                rownames(new) <- years
+                colnames(new) <- c(colnames(x), colnames(y))
+            } else if (dim.x1 > 0) {
+                years <- rownames(x)
+                new <- matrix(NA, nrow = dim.x1, ncol = dim2)
+                new[, seq_len(dim.x2)] <- x
+                rownames(new) <- years
+                colnames(new) <- c(colnames(x), colnames(y))
+            } else if (dim.y1 > 0) {
+                years <- rownames(y)
+                new <- matrix(NA, nrow = dim.y1, ncol = dim2)
+                new[, seq_len(dim.x2)] <- y
+                rownames(new) <- years
+                colnames(new) <- c(colnames(x), colnames(y))
+            } else {
+                new <- matrix(NA, nrow = 0, ncol = dim2)
+                colnames(new) <- c(colnames(x), colnames(y))
+            }
             new
-        } else if(dim.y2 == 0) {
+        } else if (dim.y2 == 0) {
             x
         } else {
             y
