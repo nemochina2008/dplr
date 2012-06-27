2cee5357af65af2bedb3b21b5b90f5489dda7c09
mvkorpel <mvkorpel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-27 07:04:07 +0000

    removing unwanted directory
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dplr@627 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tags/dplR-1.5.5/tags/dplR-1.3/R/tbrm.R
+++
@@ -1,7 +0,0 @@
-`tbrm` <-
-function(x,C=9)
-{
-  x <- as.double(x[!is.na(x)])
-  .C(dplR.tbrm,
-     x, length(x), as.double(C), result=NaN, NAOK=TRUE, DUP=FALSE)$result
-}
--- tags/dplR-1.5.5/tags/dplR-1.4.9/R/sens2.R
+++
@@ -1,6 +0,0 @@
-`sens2` <- function(x)
-{
-    y <- as.double(x[!is.na(x)])
-    .C(dplR.sens2,
-       y, as.integer(length(y)), result=NaN, NAOK=TRUE, DUP=FALSE)$result
-}
