171a15936cf0b5dad5474e85706f95c0d5f915e2
mvkorpel <mvkorpel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-28 08:22:03 +0000

    dplR 1.6.0
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dplr@839 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ tags/dplR-1.6.0/src/exactmean.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#include "dplR.h"
+#include "exactsum.h"
+
+SEXP exactmean(SEXP x){
+    SEXP ans;
+    listnode expansion;
+    size_t n;
+    expansion.next = NULL;
+    n = dplRlength(x);
+    ans = PROTECT(allocVector(REALSXP, 1));
+    /* Note: x must be a numeric vector */
+    REAL(ans)[0] = msum(REAL(x), n, &expansion) / n;
+    UNPROTECT(1);
+    return ans;
+}
